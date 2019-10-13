2f647c3ba0bca2b2a479be247ce2be8eb5d61a27
Fredrik Glendrange <Fredrik@Glendrange.com>
2019-10-13 21:01:10 +0200

    Fix the parent target issue

--- Upcoming Deadlines/src/components/ReportItem.js
+++ Upcoming Deadlines/src/components/ReportItem.js
@@ -6,7 +6,7 @@ class ReportItem extends React.Component {
     render(){
         let {reportName, untillDeadline, dueDate, reportLink, contextPath} = this.props;
         
-        return <a href={contextPath + reportLink}>
+        return <a href={contextPath + reportLink} target="_parent">
             <li>
                 <div id="date">{dueDate} - </div>
                 <div id="days" style={{color: (untillDeadline>3) ? 'black' : 'red'}}>in {untillDeadline} days</div>
